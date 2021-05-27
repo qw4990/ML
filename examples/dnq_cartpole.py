da752f6a291a652c1a7020f4eaab145713d5227e
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2021-05-27 09:15:16 +0800

    update dnq_cartpole

--- examples/dnq_cartpole.py
+++ examples/dnq_cartpole.py
@@ -5,6 +5,14 @@ import torch.nn as nn
 import math
 from dqn import DQN
 
+# Description:
+#     A pole is attached by an un-actuated joint to a cart, which moves along
+#     a frictionless track. The pendulum starts upright, and the goal is to
+#     prevent it from falling over by increasing and reducing the cart's
+#     velocity.
+# Source:
+#     This environment corresponds to the version of the cart-pole problem
+#     described by Barto, Sutton, and Anderson
 # Observation:
 #     Type: Box(4)
 #     Num     Observation               Min                     Max
@@ -17,19 +25,32 @@ from dqn import DQN
 #     Num   Action
 #     0     Push cart to the left
 #     1     Push cart to the right
+#     Note: The amount the velocity that is reduced or increased is not
+#     fixed; it depends on the angle the pole is pointing. This is because
+#     the center of gravity of the pole increases the amount of energy needed
+#     to move the cart underneath it
 # Reward:
 #     Reward is 1 for every step taken, including the termination step
+# Starting State:
+#     All observations are assigned a uniform random value in [-0.05..0.05]
+# Episode Termination:
+#     Pole Angle is more than 12 degrees.
+#     Cart Position is more than 2.4 (center of the cart reaches the edge of
+#     the display).
+#     Episode length is greater than 200.
+#     Solved Requirements:
+#     Considered solved when the average return is greater than or equal to
+#     195.0 over 100 consecutive trials.
 env = gym.make('CartPole-v0')
-state_dim = 4
-action_dim = 2
-
-
 dnq = DQN(4, 2)
 for i_epi in range(80):
     state0 = env.reset()
     for i_round in range(200):
         action = dnq.act(state0)
         state1, reward, done, info = env.step(action)
+
+        # TODO: adjust the reward according to the pole angle
+
         if done:
             reward = -10
         
